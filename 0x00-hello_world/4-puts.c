5294d6fe2a02d4456f818ce98032e9e43b8fe49c
yela-man <yelamanthegreat@gmail.com>
2022-12-08 13:37:48 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry Point
- *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    puts("\"programming is like a game");
-}    
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+}     

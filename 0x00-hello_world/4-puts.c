3c649eaa9a8d3adc46765cf27dc5274ee071f705
yela-man <yelamanthegreat@gmail.com>
2022-12-08 14:18:23 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - main block
  * Return: o
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }     

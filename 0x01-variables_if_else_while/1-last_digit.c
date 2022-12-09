c256097e6eb5b05c949da2d27153516eb1381201
yela-man <yelamanthegreat@gmail.com>
2022-12-09 15:57:15 -0800

    task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,45 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes here*/
+#include <stdlib.h>                                                                                                                           
 
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n, lastnum;
+#include <time.h>                                                                                                                             
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastnum = n % 10;
-	if (lastNum > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
-	else if (lastNum == 0)
-		 printf("Last digit of %d is %d and is 0\n", n, lastNum);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
-	return (0); }
+#include <stdio.h>                                                                                                                            
+
+/* more headers goes here*/                                                                                                                   
+
+                                                                                                                                              
+
+/**                                                                                                                                           
+ *
+ *  * main - Entry point                                                                                                                         
+ *
+ *   * Return: Always 0 (Success)                                                                                                                 
+ *
+ *    */                                                                                                                                           
+
+int main(void)                                                                                                                                
+
+{                                                                                                                                             
+
+	        int n, lastnum;                                                                                                                       
+
+		                                                                                                                                              
+
+		        srand(time(0));                                                                                                                       
+
+			        n = rand() - RAND_MAX / 2;                                                                                                            
+
+				        lastnum = n % 10;                                                                                                                     
+
+					        if (lastNum > 5)                                                                                                                      
+
+							                printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);                                                         
+
+						        else if (lastNum == 0)                                                                                                                
+
+								                 printf("Last digit of %d is %d and is 0\n", n, lastNum);                                                                     
+
+							        else                                                                                                                                  
+
+									                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);                                                  
+
+								        return (0); }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+		putchar(ch);
+		ch++;
+         putchar('\n');
+        return (0); }	 

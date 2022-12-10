f199b562dd488ac236c4934da108716445d472d2
yela-man <yelamanthegreat@gmail.com>
2022-12-09 16:29:56 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,45 +1,23 @@
-#include <stdlib.h>                                                                                                                           
-
+#include <stdlib.h>                                                                                                                          
 #include <time.h>                                                                                                                             
-
-#include <stdio.h>                                                                                                                            
-
-/* more headers goes here*/                                                                                                                   
-
-                                                                                                                                              
-
-/**                                                                                                                                           
- *
- *  * main - Entry point                                                                                                                         
- *
- *   * Return: Always 0 (Success)                                                                                                                 
- *
- *    */                                                                                                                                           
-
-int main(void)                                                                                                                                
-
-{                                                                                                                                             
-
-	        int n, lastnum;                                                                                                                       
-
-		                                                                                                                                              
-
-		        srand(time(0));                                                                                                                       
-
-			        n = rand() - RAND_MAX / 2;                                                                                                            
-
-				        lastnum = n % 10;                                                                                                                     
-
-					        if (lastNum > 5)                                                                                                                      
-
-							                printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);                                                         
-
-						        else if (lastNum == 0)                                                                                                                
-
-								                 printf("Last digit of %d is %d and is 0\n", n, lastNum);                                                                     
-
-							        else                                                                                                                                  
-
-									                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);                                                  
-
-								        return (0); }
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digits,compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int last;
+      
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	return (0); }

307443224cba351e494e09ce9eddcb2d6980fb01
yela-man <yelamanthegreat@gmail.com>
2022-12-09 17:06:42 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: o
+ */
 int main(void)
 {
 	 int n;
@@ -17,4 +20,5 @@ int main(void)
 		  printf("Last digit of %i is %i and is 0\n", n, last);
 	  else if (last < 6)
 		   printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
-	   return (0); }
+	   return (0);
+}	   

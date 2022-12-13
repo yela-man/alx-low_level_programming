5b3f9584f4ea844d61e408b29ebc6d391b140489
yela-man <yelamanthegreat@gmail.com>
2022-12-13 14:03:06 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return: 1 if character is a letter, 0 otherwise
+ * Return: 1 if character is a letter,0 otherwise
  */
 int _isalpha(int c)
 {

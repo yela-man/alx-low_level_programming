41235fbfaea8593980aef6a9ecd0b2165dcc9aa6
yela-man <yelamanthegreat@gmail.com>
2022-12-13 16:11:12 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,11 +14,12 @@ void times_table(void)
 	while (a <= 9)
 	{
 		b = 0;
-		while (b <=9)
+		while (b <= 9)
 		{
 			rep = a * b;
 			if (b == 0)
 			{
+				_putchar(',')
 				_putchar('0' +  rep);
 			}
 			else if (rep < 10)

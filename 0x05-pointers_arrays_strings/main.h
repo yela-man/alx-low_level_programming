077362eedc540001bf19091544a9b6a64663b181
yela-man <yelamanthegreat@gmail.com>
2022-12-19 07:23:13 -0800

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-void puts half(char *str);
+void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 

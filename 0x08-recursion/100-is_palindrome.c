23380553a6ac487cc24538a7cbd354ce46d7273d
yela-man <yelamanthegreat@gmail.com>
2023-01-04 14:01:04 -0800

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@ int _strlen_recursion(char *s);
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
  *
- * int is_palindrome(char *s)
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {

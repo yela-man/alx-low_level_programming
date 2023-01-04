b5e4134d57a498be2debdfc1855b2e078015068a
yela-man <yelamanthegreat@gmail.com>
2023-01-04 13:26:08 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,14 +29,14 @@ int find_sqrt(int num, int root)
  * Return: If n has a natural square root - the natural square root of n
  * If n does not have a natural square root - -1
  */
-int _sqrt_recursion(int n) 
+int _sqrt_recursion(int n)
 {
 	int root = 0;
 
-	if (n < 0) 
+	if (n < 0)
 		return (-1);
 
-	if (n == 1) 
+	if (n == 1)
 		return (1);
 
 	return (find_sqrt(n, root));

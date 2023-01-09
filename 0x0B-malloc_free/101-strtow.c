fdadb7ebd14c8625666a2822e7b9adfff1333088
yela-man <yelamanthegreat@gmail.com>
2023-01-09 14:07:36 -0800

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -71,23 +71,17 @@ char **strtow(char *str)
 
 				if (tmp == NULL)
 					return (NULL);
-
 				while (start < end)
 					*tmp++ = str[start++];
-
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
 				c = 0;
 			}
 		}
-		
 		else if (c++ == 0)
 			start = i;
 	}
-	
 	matrix[k] = NULL;
 	return (matrix);
 }
-
-

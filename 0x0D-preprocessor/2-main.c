a543b6491cd9d05986f272a6f5797b82fc206abf
yela-man <yelamanthegreat@gmail.com>
2023-01-16 12:06:01 -0800

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - prints the name of the file
+ * main - prints the name of file.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);

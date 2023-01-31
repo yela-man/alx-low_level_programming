8d43e5ab88c7de2efb74d0d79d8c63f85a118a63
yela-man <yelamanthegreat@gmail.com>
2023-01-31 06:02:15 -0800

    tasks of singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -18,20 +18,20 @@ size_t free_listint_safe(listint_t **h)
 	while (*h)
 	{
 		diff = *h - (*h)->next;
-		 if (diff > 0)
-		 {
-			 temp = (*h)->next;
-			 free(*h);
-			 *h = temp;
-			 len++;
-		 }
-		 else
-		 {
-			 free(*h);
-			 *h = NULL;
-			 len++;
-			 break;
-		 }
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
 	}
 
 	*h = NULL;
